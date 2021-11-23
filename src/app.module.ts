7c9149756a8b93ea23ca431a6958055be29f6f15
S-011 <u201811286@upc.edu.pe>
2021-11-22 22:38:52 -0500

    BillingModule added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PsychologistsModule } from './psychologists/psychologists.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PatientsModule } from "./patients/patients.module";
+import { BillingsModule } from "./billings/billings.module";
 
 @Module({
-  imports: [PsychologistsModule, TypeOrmModule.forRoot(), PatientsModule],
+  imports: [PsychologistsModule, TypeOrmModule.forRoot(), PatientsModule, BillingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
